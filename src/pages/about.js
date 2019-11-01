961f79f878fb8fe77385aed34e345e4731906353
Jackson Oppenheim <jacksonopp@gmail.com>
2019-11-01 11:39:41 -0500

    added styling and header/layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Layout from "../components/layout";
+
 export default () => (
-  <>
+  <Layout>
     <h1>About Me</h1>
     <p>My personal website</p>
     <Link to="/">&larr; Back to home</Link>
-  </>
+  </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import Layout from "../components/layout";
+
 export default () => (
-    <>
+    <Layout>
         <h1>Home</h1>
         <p>Hello everyone</p>
         <Link to="/about">learn about me &rarr;</Link>
-    </>
+    </Layout>
 )
