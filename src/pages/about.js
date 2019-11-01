3ff5d008892739858d182b65ccab5600ad44f9ad
Jackson Oppenheim <jacksonopp@gmail.com>
2019-11-01 11:00:26 -0500

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About Me</h1>
+    <p>My personal website</p>
+    <Link to="/">&larr; Back to home</Link>
+  </>
+)
